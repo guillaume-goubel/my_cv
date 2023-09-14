ccf4b469b11076d3db716cfeb6c8f87c0e6e476b
guillaume <guillaume.goubel.pro@gmail.com>
2023-09-14 20:22:36 +0200

    add base template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(Request $request): Response
     {
+        $locale = $request->getLocale();
+                
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
