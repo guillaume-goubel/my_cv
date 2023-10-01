3bccab5e0ea09f8b7bf6c71e64711eee56af65b3
guillaume <guillaume.goubel.pro@gmail.com>
2023-10-01 19:40:28 +0200

    test redirection on server 2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,12 +12,12 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home', methods: ['GET'])]
     public function index(Request $request): Response
     {
-        // $uri = $request->getRequestUri();
-        // if (empty($uri) || $uri === '/') {
-        //     $locale = $request->getLocale();
-        //     $url = $this->generateUrl('app_home', ['_locale' => $locale]);
-        //     return $this->redirect($url);
-        // }
+        $uri = $request->getRequestUri();
+        if (empty($uri) || $uri === '/') {
+            $locale = $request->getLocale();
+            $url = $this->generateUrl('app_home', ['_locale' => $locale]);
+            return $this->redirect($url);
+        }
           
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
