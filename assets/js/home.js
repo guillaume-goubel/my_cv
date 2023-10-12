0581c7d072876e8b80a2d66c54e12d98beb5190d
guillaume <guillaume.goubel.pro@gmail.com>
2023-10-12 19:54:05 +0200

    add experience

--- assets/js/home.js
+++ assets/js/home.js
@@ -13,13 +13,27 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    // SET DARK/LIGHT THEME
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+        document.getElementById('theme-choice').checked = true;
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+    }else{
+        document.getElementById('theme-choice').checked = false;
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+    }
+
 });
 
+// ABOUT COMPLEMENT
 document.getElementById("about-complement-dot").addEventListener("click", (event) => {
     event.preventDefault();
     document.getElementById('about-complement-container').classList.toggle('hidden');
 });
 
+// LANGUAGE CHOICE
 document.getElementById("language-choice-select").addEventListener("change", (event) => {
     event.preventDefault();
     const path = event.target.value;
@@ -35,9 +49,11 @@ document.getElementById("theme-choice").addEventListener("change", (event) => {
     if (event.target.checked) {       
         document.body.classList.remove('light');
         document.body.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
     } else {
         document.body.classList.remove('dark');
         document.body.classList.add('light');
+        localStorage.setItem('theme', 'light');
     }
 
 });
